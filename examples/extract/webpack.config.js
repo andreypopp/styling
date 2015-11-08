eae282a75499cc589dbdd18071c351731b5e10b9
Peter Gassner <peter@naehrstoff.ch>
2015-11-08 17:03:32 +0100

    Example of how to use the Postcss nesting plugin
    This example shows how additional Postcss plugins can be used by using the Postcss loader and specifying plugins it should use.
    
    This is likely an unnecessary step, because Postcss will run twice with this setup. Instead, it should be possible to specify Postcss plugins that Styling should use directly when it does the transformation.

--- examples/extract/webpack.config.js
+++ examples/extract/webpack.config.js
@@ -1,5 +1,6 @@
 var styling = require('styling');
 var ExtractTextWebpackPlugin = require('extract-text-webpack-plugin');
+var postcssNested = require('postcss-nested');
 
 module.exports = {
 
@@ -14,13 +15,17 @@ module.exports = {
     loaders: [
       {
         include: /\.style\.js$/,
-        loader: styling(ExtractTextWebpackPlugin.extract('style', 'css?modules'), 'babel')
+        loader: styling(ExtractTextWebpackPlugin.extract('style', 'css?modules!postcss'), 'babel')
       },
       {
         include: /\.js$/,
         loader: 'babel'
       }
     ]
+  },
+
+  postcss: function() {
+    return [postcssNested];
   },
 
   plugins: [
